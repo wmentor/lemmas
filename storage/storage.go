5497710f27f0b21ee7d2182be8710d34e9f3c033
Mikhail Kirillov <wmentor@mail.ru>
2020-09-20 09:18:44 +0300

    storage loader

--- storage/storage.go
+++ storage/storage.go
@@ -3,6 +3,9 @@ package storage
 import (
 	"bufio"
 	"io"
+	"strings"
+
+	"github.com/wmentor/tokens"
 )
 
 var (
@@ -14,10 +17,24 @@ func Load(in io.Reader) {
 	br := bufio.NewReader(in)
 	res := map[string]string{}
 
+	var list []string = make([]string, 0, 10)
+
+	fn := func(t string) {
+		list = append(list, t)
+	}
+
 	for {
 		str, err := br.ReadString('\n')
 		if err != nil && str == "" {
 			break
+		}
+
+		list = list[:0]
+
+		tokens.Process(strings.NewReader(str), fn)
+
+		if len(list) > 1 {
+			res[list[0]] = strings.Join(list[1:], "|")
 		}
 	}
 
