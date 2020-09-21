7bc1664676230f1a136e436faedfaa64a117ef19
Mikhail Kirillov <wmentor@mail.ru>
2020-09-21 22:50:47 +0300

    issue #5 save data to writer

--- storage/storage.go
+++ storage/storage.go
@@ -2,7 +2,9 @@ package storage
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"sort"
 	"strings"
 
 	"github.com/wmentor/lemmas/log"
@@ -35,11 +37,29 @@ func Load(in io.Reader) {
 		tokens.Process(strings.NewReader(str), fn)
 
 		if len(list) > 1 {
-			res[list[0]] = strings.Join(list[1:], "|")
+			res[list[0]] = strings.Join(list[1:], " ")
 		} else if len(list) == 1 {
 			log.Log("ERROR", "invalid string: %s", strings.TrimSpace(str))
 		}
 	}
 
 	data = res
+}
+
+func Save(out io.Writer) {
+	bw := bufio.NewWriter(out)
+	defer bw.Flush()
+
+	list := make([]string, 0, len(data))
+
+	for k := range data {
+		list = append(list, k)
+	}
+
+	sort.Sort(sort.StringSlice(list))
+
+	for _, k := range list {
+		v := data[k]
+		fmt.Fprintf(bw, "%s %s\n", k, v)
+	}
 }
