d7e47b47e44931f026cd5c836ba22cbc43a33a3e
Mikhail Kirillov <wmentor@mail.ru>
2020-10-15 19:25:29 +0300

    issue #20 process text web interface

--- server/controller/controller.go
+++ server/controller/controller.go
@@ -2,5 +2,6 @@ package controller
 
 import (
 	_ "github.com/wmentor/lemmas/server/controller/forms"
+	_ "github.com/wmentor/lemmas/server/controller/lemmas"
 	_ "github.com/wmentor/lemmas/server/controller/text"
 )
