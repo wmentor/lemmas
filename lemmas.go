4a96e5fdf65fd858f695515a63169dae9c5679b5
Mikhail Kirillov <wmentor@mail.ru>
2020-10-22 18:42:21 +0300

    issue #25 tiny forms support

--- lemmas.go
+++ lemmas.go
@@ -13,6 +13,7 @@ import (
 var (
 	filename string
 	signs    map[string]bool
+	tinyMap  map[string]string
 )
 
 func init() {
@@ -35,6 +36,77 @@ func init() {
 		")":  true,
 		"{":  true,
 		"}":  true,
+	}
+
+	tinyMap = map[string]string{
+		"a":        "a",
+		"by":       "by",
+		"i":        "i",
+		"ii":       "ii",
+		"iii":      "iii",
+		"in":       "in",
+		"into":     "into",
+		"iv":       "iv",
+		"ix":       "ix",
+		"the":      "the",
+		"to":       "to",
+		"v":        "v",
+		"vi":       "vi",
+		"vii":      "vii",
+		"viii":     "viii",
+		"x":        "x",
+		"а":        "а",
+		"ах":       "ах",
+		"без":      "без",
+		"бы":       "бы",
+		"в":        "в",
+		"ведь":     "ведь",
+		"во":       "в",
+		"вот":      "вот",
+		"где":      "где",
+		"где-то":   "где-то",
+		"даже":     "даже",
+		"для":      "для",
+		"если":     "если",
+		"же":       "же",
+		"за":       "за",
+		"и":        "и",
+		"или":      "или",
+		"из":       "из",
+		"к":        "к",
+		"как":      "как",
+		"как-то":   "как-то",
+		"км":       "км",
+		"когда":    "когда",
+		"когда-то": "когда-то",
+		"кто":      "кто",
+		"кто-то":   "кто-то",
+		"ли":       "ли",
+		"либо":     "или",
+		"мм":       "мм",
+		"на":       "на",
+		"над":      "над",
+		"не":       "не",
+		"ни":       "ни",
+		"но":       "но",
+		"о":        "о",
+		"об":       "о",
+		"обо":      "о",
+		"по":       "по",
+		"под":      "под",
+		"с":        "с",
+		"см":       "см",
+		"со":       "с",
+		"так":      "так",
+		"также":    "также",
+		"то":       "то",
+		"тогда":    "тогда",
+		"тоже":     "тоже",
+		"у":        "у",
+		"что":      "что",
+		"что-то":   "что-то",
+		"чтоб":     "чтобы",
+		"чтобы":    "чтобы",
 	}
 }
 
@@ -72,7 +144,21 @@ func DelForm(form string) {
 }
 
 func CanProcess(form string) bool {
+
 	if signs[form] {
+		return true
+	}
+
+	if _, ok := tinyMap[form]; ok {
+		return true
+	}
+
+	return canProcess(form)
+}
+
+func canProcess(form string) bool {
+
+	if form == "" {
 		return true
 	}
 
@@ -87,7 +173,7 @@ func CanProcess(form string) bool {
 	for idx, run := range form {
 		if idx != 0 {
 			if run == '-' {
-				return CanProcess(form[:idx]) && CanProcess(form[idx+1:])
+				return canProcess(form[:idx]) && canProcess(form[idx+1:])
 			} else {
 				suf := form[idx:]
 				if storage.Has(suf) {
@@ -128,6 +214,19 @@ func ProcessForm(form string) string {
 		return form
 	}
 
+	if res, ok := tinyMap[form]; ok {
+		return res
+	}
+
+	return processForm(form)
+}
+
+func processForm(form string) string {
+
+	if form == "" {
+		return ""
+	}
+
 	if v := storage.GetRaw(form); v != "" {
 		return v
 	}
@@ -136,8 +235,8 @@ func ProcessForm(form string) string {
 		if pos > 0 {
 
 			if run == '-' {
-				f1 := ProcessForm(form[:pos])
-				f2 := ProcessForm(form[pos+1:])
+				f1 := processForm(form[:pos])
+				f2 := processForm(form[pos+1:])
 
 				fl1 := strings.Split(f1, " ")
 				fl2 := strings.Split(f2, " ")
