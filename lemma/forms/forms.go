e71a2a227a88e3a2bb7acef4de9f98676d75fcb7
Mikhail Kirillov <wmentor@mail.ru>
2020-10-29 22:10:10 +0300

    issue #34 forms index

---
+++ lemma/forms/forms.go
@@ -0,0 +1,57 @@
+package forms
+
+import (
+	"strings"
+)
+
+const (
+	sep string = "|"
+)
+
+var (
+	data map[string]string = map[string]string{}
+)
+
+func Add(src string, srcFlags, base string, baseFlags string) {
+
+	if src == "" || base == "" {
+		return
+	}
+
+	maker := strings.Builder{}
+
+	if str, has := data[src]; has {
+		maker.WriteString(str)
+		maker.WriteString(sep)
+	}
+
+	if srcFlags != "" {
+		maker.WriteString(srcFlags)
+	} else {
+		maker.WriteRune('0')
+	}
+
+	maker.WriteString(sep)
+	maker.WriteString(base)
+	maker.WriteString(sep)
+
+	if baseFlags != "" {
+		maker.WriteString(baseFlags)
+	} else {
+		maker.WriteRune('0')
+	}
+
+	data[src] = maker.String()
+}
+
+func Get(src string) ([]string, bool) {
+	if res, ok := data[src]; ok {
+		return strings.Split(res, sep), true
+	}
+	return nil, false
+}
+
+func Has(src string) bool {
+	_, has := data[src]
+	return has
+}
---
+++ lemma/forms/forms_test.go
@@ -0,0 +1,28 @@
+package forms
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestForms(t *testing.T) {
+
+	tA := func(src, srcF, base, baseF string, wait string) {
+		Add(src, srcF, base, baseF)
+		if src != "" && !Has(src) {
+			t.Fatalf("Add failed for: %s", src)
+		}
+		if list, _ := Get(src); strings.Join(list, sep) != wait {
+			t.Fatalf("Get failed for: %s", src)
+		}
+	}
+
+	tA("t1", "1", "t2", "2", "1|t2|2")
+	tA("t1", "", "t3", "", "1|t2|2|0|t3|0")
+	tA("", "1", "2", "", "")
+
+	if _, ok := Get("12312313"); ok {
+		t.Fatal("Get unknown value failed")
+	}
+
+}
