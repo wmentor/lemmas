3e3774a0e69539452ac147354c622db43be2d8fc
Mikhail Kirillov <wmentor@mail.ru>
2020-10-29 22:09:33 +0300

    issue #34 form flags

---
+++ lemma/flags/flags_test.go
@@ -0,0 +1,25 @@
+package flags
+
+import (
+	"testing"
+)
+
+func TestFlags(t *testing.T) {
+
+	tF := func(txt string, flag Flag) {
+		f := New(txt)
+		if f != flag {
+			t.Fatalf("New failed for: %s", txt)
+		}
+		if flag.String() != txt {
+			t.Fatalf("String failed for: %s", txt)
+		}
+	}
+
+	tF("", 0)
+	tF("mr", F_MR)
+	tF("noun.sg.mr.vp", F_NOUN|F_SG|F_MR|F_VP)
+	tF("noun.mg.sr.pp", F_NOUN|F_MG|F_SR|F_PP)
+	tF("noun.lname", F_NOUN|F_LNAME)
+	tF("roman", F_ROMAN)
+}
