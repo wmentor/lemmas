e552de77b375ca9ec941b3145f91090c2e0db364
Mikhail Kirillov <wmentor@mail.ru>
2020-11-04 15:18:59 +0300

    issue #34 forms sructure

--- lemma/forms/forms_test.go
+++ lemma/forms/forms_test.go
@@ -1,33 +1,21 @@
 package forms
 
 import (
-	"strings"
 	"testing"
 )
 
 func TestForms(t *testing.T) {
 
-	tA := func(src, srcF, base, baseF string, wait string) {
-		Add(src, srcF, base, baseF)
-		if src != "" && !Has(src) {
-			t.Fatalf("Add failed for: %s", src)
+	tF := func(txt string) {
+		f := New(txt)
+		if f == nil {
+			t.Fatalf("New failed for: %s", txt)
 		}
-		if list, _ := Get(src); strings.Join(list, sep) != wait {
-			t.Fatalf("Get failed for: %s", src)
+
+		if f.String() != txt {
+			t.Fatalf("String failed for: %s", txt)
 		}
 	}
 
-	tA("t1", "1", "t2", "2", "1|t2|2")
-	tA("t1", "", "t3", "", "1|t2|2|0|t3|0")
-	tA("", "1", "2", "", "")
-
-	if _, ok := Get("12312313"); ok {
-		t.Fatal("Get unknown value failed")
-	}
-
-	Reset()
-
-	if _, ok := Get("t1"); ok {
-		t.Fatal("Get unknown value failed")
-	}
+	tF("тест:ru.noun.sg.mr.ip")
 }
